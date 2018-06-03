2630a8109a919b35f10750f099c21c0fd985002c
Minusi <minusie357@gmail.com>
2018-06-03 23:22:19 +0900

    언리얼 프로젝트 레포지토리에 초기 적재
    # 세부사항
    % Basic Code, Desktop & Console, Including Starter Contents

---
+++ Source/DanmakuProjectEditor.Target.cs
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class DanmakuProjectEditorTarget : TargetRules
+{
+	public DanmakuProjectEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+
+		ExtraModuleNames.AddRange( new string[] { "DanmakuProject" } );
+	}
+}
