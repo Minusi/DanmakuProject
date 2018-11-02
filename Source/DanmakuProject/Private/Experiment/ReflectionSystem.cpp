76240b074d5e16bea78a44fc0dc59c112b93bb62
Minusi <minusie357@gmail.com>
2018-11-02 19:38:44 +0900

    CHANGE _ 언리얼 테스팅 환경을 위한 Experiment 디렉토리 구성
    # 세부사항
    1. Experiment 디렉토리 구성
      - Map_Experiment 애셋 생성
      - 라이트매스를 경험하기 위한 각종 애셋( 애트머스패릭 포그, 디렉셔널 라이트 등의 라이팅 컴포넌트 ) 배치
      - AI 학습을 위한 블랙보드 및 비헤이비어 트리 애셋 생성
      - 그 외 잡다한 것들

---
+++ Source/DanmakuProject/Private/Experiment/ReflectionSystem.cpp
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ReflectionSystem.h"
+#include "Engine/Engine.h"
+
+
+// Sets default values
+AReflectionSystem::AReflectionSystem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AReflectionSystem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	UClass* StaticClassPtr = AReflectionSystem::StaticClass();
+	UClass* GetClassPtr = this->GetClass();
+	UClass* GetClassFromStaticClassPtr = AReflectionSystem::StaticClass()->GetClass();
+	UClass* GetClassToGetClassFromStaticClassPtr = AReflectionSystem::StaticClass()->GetClass()->GetClass();
+	UClass* GCTGCTGetClassFormStaticClassPtr = AReflectionSystem::StaticClass()->GetClass()->GetClass()->GetClass();
+
+	FString SCPString = StaticClassPtr->GetName();
+	FString GCPString = GetClassPtr->GetName();
+	FString GCFSCPString = GetClassFromStaticClassPtr->GetName();
+	FString GCTGCFSCP = GetClassToGetClassFromStaticClassPtr->GetName();
+	FString GCTGCTGCFSCP = GCTGCTGetClassFormStaticClassPtr->GetName();
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, SCPString);
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, GCPString);
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Blue, GCFSCPString);
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Blue, GCTGCFSCP);
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Blue, GCTGCTGCFSCP);
+	}
+}
+
+// Called every frame
+void AReflectionSystem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
